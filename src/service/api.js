f62494c3029f9830cf65aabc75a6e0ee3be526b5
sergiop45 <sergiopsantos33@gmail.com>
2023-03-11 10:20:26 -0300

    url-api

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const baseUrl = axios.create({
-    baseURL: 'http://localhost:4000/api'
+    baseURL: 'https://api-gitvagas.up.railway.app/api'
 });
 
 export default baseUrl;
